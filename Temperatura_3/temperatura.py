2a7ff2f312ad45088c2fe8d06d6f42ac9dd083cf
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-24 20:11:33 -0600

    Add files via upload

---
+++ Temperatura_3/temperatura.py
@@ -0,0 +1,10 @@
+print (" ") #Espacio a agregar
+print ("Lira Hernandez Dayana Yamileth: programa que convierte Fahrenheit en celsius")
+print (" ")
+
+a = float(input("Ingresa la temperatura en escala Fahrenheit"))
+#Instruccion para ingresar la temperatura
+celsius = (5/9) * (a-32) #Operacion a realizar
+print ("El resultado es:", celsius) #Resultado de la operacion es
+
+print (" ")  #Espacio a agregar
